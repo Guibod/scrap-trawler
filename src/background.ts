b1158b0bff98a2520ecd61c3f7f2e971f4e92a47
Guillaume Boddaert <gboddaert@solocal.com>
2025-02-15 11:26:03 +0100

    chore: wip soon to be replaced by graphql variant

--- src/background.ts
+++ src/background.ts
@@ -1,22 +1,24 @@
+import { isScrapeUrlMessage, isScrapeStartMessage, isScrapeUrlCompletedMessage } from "~scripts/types/message-types"
+import { EventScraper } from "~scripts/event-scraper"
+
 console.log("Scrap Trawler background script is running.");
+
+const scrapers: Map<string, EventScraper> = new Map();
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   console.log("Received message in background:", message);
 
-  if (message.action === "START_SCRAPE") {
-    console.log("Forwarding START_SCRAPE to content script...");
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0 && tabs[0].id) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "START_SCRAPE" });
-      }
-    });
+  if (isScrapeStartMessage(message)) {
+     const currentTab = await chrome.tabs.query({ active: true, currentWindow: true })
+
+    const scraper = new EventScraper(message.url, currentTab[0].id);
+    scrapers.set(scraper.url, scraper);
+
+    scraper.scrap();
     sendResponse({ status: "Scrape initiated" });
+    return new Promise(() => true);
   }
 
-  if (message.action === "SCRAPE_COMPLETE") {
-    console.log("Forwarding SCRAPE_COMPLETE to UI...");
-    chrome.runtime.sendMessage({ action: "SCRAPE_COMPLETE", data: message.data });
-  }
-
+  console.log("Unknown message type:", message);
   return true; // Keep service worker alive
 });
