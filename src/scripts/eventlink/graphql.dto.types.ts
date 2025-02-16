f0d01ed5ea38aaa57de37da62c4c833cd524e961
Guillaume Boddaert <guibod12@gmail.com>
2025-02-16 18:53:12 +0100

    feat: complete tournament extraction that properly reaches all layers

--- src/scripts/eventlink/graphql.dto.types.ts
+++ src/scripts/eventlink/graphql.dto.types.ts
@@ -1,4 +1,4 @@
-export interface EventData {
+export interface EventResponse {
   event: EventDetails;
 }
 
@@ -124,4 +124,197 @@ export interface InfractionCategory {
 export interface Penalty {
   id: string;
   name: string;
+}
+
+export interface GameStateResponse {
+  gameStateV2AtRound: GameState;
+}
+
+export interface GameState {
+  eventId: string;
+  minRounds: number;
+  podPairingType: string;
+  draft: Draft | null;
+  playoffDraft: Draft | null;
+  deckConstruction: DeckConstruction;
+  currentRoundNumber: number;
+  rounds: Round[];
+  drops: Drop[];
+  nextRoundMeta: NextRoundMeta;
+  gamesToWin: number;
+  teams: GameStateTeam[];
+  playoffRounds: number;
+}
+
+export interface Draft {
+  timerId: string;
+  pods: Pod[];
+}
+
+export interface Pod {
+  podNumber: number;
+  seats: Seat[];
+}
+
+export interface Seat {
+  seatNumber: number;
+  teamId: string;
+}
+
+export interface DeckConstruction {
+  timerId: string;
+  seats: Seat[];
+}
+
+export interface Round {
+  roundId: string;
+  roundNumber: number;
+  isFinalRound: boolean;
+  isPlayoff: boolean;
+  isCertified: boolean;
+  pairingStrategy: string;
+  canRollback: boolean;
+  timerId: string;
+  matches: Match[];
+  standings: Standing[];
+}
+
+export interface Match {
+  matchId: string;
+  isBye: boolean;
+  teamIds: string[];
+  tableNumber: number;
+  results: Result[];
+}
+
+export interface Result {
+  isBye: boolean;
+  wins: number;
+  losses: number;
+  draws: number;
+  teamId: string;
+}
+
+export interface Standing {
+  teamId: string;
+  rank: number;
+  wins: number;
+  losses: number;
+  draws: number;
+  matchPoints: number;
+  gameWinPercent: number;
+  opponentGameWinPercent: number;
+  opponentMatchWinPercent: number;
+}
+
+export interface Drop {
+  teamId: string;
+  roundNumber: number;
+}
+
+export interface NextRoundMeta {
+  hasDraft: boolean;
+  hasDeckConstruction: boolean;
+}
+
+export interface GameStateTeam {
+  teamId: string;
+  teamName: string;
+  tableNumber: number;
+  players: Player[];
+}
+
+export interface Player {
+  personaId: string;
+  displayName: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface OrganizationResponse {
+  organization: Organization;
+}
+
+export interface Organization {
+  id: string;
+  name: string;
+  groups: Group[];
+  latitude: number;
+  longitude: number;
+  phoneNumber: string | null;
+  emailAddress: string | null;
+  postalAddress: string | null;
+  isPremium: boolean;
+  acceptedTermsAndConditionsAt: string | null;
+  availableTemplates: Template[];
+  venues: Venue[];
+  brands: string[];
+  roles: Role[];
+}
+
+export interface Group {
+  id: string;
+  onlineOnly: boolean;
+}
+
+export interface Template {
+  id: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  isEvergreen: boolean;
+  hasPromoProduct: boolean;
+  featured: boolean;
+  templateQuota: number;
+  wpnArticle: string | null;
+  keyArt: string | null;
+  fieldRules: FieldRule[];
+  templateType: string;
+  prereleaseType: string;
+  mustHaveWizardsAccount: boolean;
+}
+
+export interface FieldRule {
+  id: string;
+  fieldName: string;
+  rule: Rule;
+}
+
+export interface Rule {
+  rule: string;
+  value: string;
+}
+
+export interface Venue {
+  id: string;
+  name: string;
+  address: string;
+  timeZone: string;
+  phoneNumber: string | null;
+  googlePlaceId: string | null;
+  streetAddress: string | null;
+  city: string;
+  state: string;
+  country: string;
+  postalCode: string;
+  latitude: number;
+  longitude: number;
+  capacity: number;
+  emailAddress: string | null;
+  notes: string | null;
+  isApproved: boolean;
+  isDeleted: boolean;
+}
+
+export interface Role {
+  roleName: string;
+  user: User;
+}
+
+export interface User {
+  personaId: string;
+  firstName: string;
+  lastName: string;
+  displayName: string;
+  emailAddress: string;
 }
