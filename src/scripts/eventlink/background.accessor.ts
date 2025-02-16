f9162b13a8b3785057392a04a9094f9384e0399a
Guillaume Boddaert <gboddaert@solocal.com>
2025-02-16 23:14:47 +0100

    test: added more test, on background.accessor.ts and check the merge request and release ci

--- src/scripts/eventlink/background.accessor.ts
+++ src/scripts/eventlink/background.accessor.ts
@@ -1,18 +1,31 @@
-export const getAuthCookie = async (): Promise<string | null> => {
-  return new Promise((resolve) => {
-    chrome.cookies.get({ url: "https://eventlink.wizards.com", name: "clientAuth" }, (cookie) => {
-      resolve(cookie ? cookie.value : null);
-    });
-  });
-};
+export class BackgroundAccessor {
+  private cookieGetter: (details: chrome.cookies.Details, callback: (cookie: chrome.cookies.Cookie | null) => void) => void;
 
-export const getAccessToken = async (): Promise<string | null> => {
-  return getAuthCookie().then((authCookie: string | null) => {
-    const authCookieJson = atob(authCookie);
-    const payload: { access_token: string} = JSON.parse(authCookieJson);
-    return payload.access_token;
-  }).catch(async (e) => {
-    console.error("Failed to access token: ", e);
-    return null;
-  });
+  constructor(
+    cookieGetter?: (details: chrome.cookies.Details, callback: (cookie: chrome.cookies.Cookie | null) => void) => void
+  ) {
+    this.cookieGetter = cookieGetter || chrome.cookies.get;
+  }
+
+  async getAuthCookie(): Promise<string | null> {
+    return new Promise((resolve) => {
+      this.cookieGetter({ url: "https://eventlink.wizards.com", name: "clientAuth" }, (cookie) => {
+        resolve(cookie ? cookie.value : null);
+      });
+    });
+  }
+
+  async getAccessToken(): Promise<string | null> {
+    return this.getAuthCookie()
+      .then((authCookie: string | null) => {
+        if (!authCookie) return null;
+        const authCookieJson = atob(authCookie);
+        const payload: { access_token: string } = JSON.parse(authCookieJson);
+        return payload.access_token;
+      })
+      .catch(async (e) => {
+        console.error("Failed to access token: ", e);
+        return null;
+      });
+  }
 }
