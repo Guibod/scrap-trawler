1a190fe19572a80a2e2a24a3ec0144c9a36e8301
Guillaume Boddaert <gboddaert@solocal.com>
2025-03-08 15:52:55 +0100

    feat: added layout to the routed pages

--- src/resources/ui/routes.tsx
+++ src/resources/ui/routes.tsx
@@ -2,12 +2,17 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import MainPage from "~resources/ui/pages/main"
 import EventPage from "~resources/ui/pages/event"
 import React from "react"
+import SettingsPage from "~resources/ui/pages/settings"
+import MainLayout from "~resources/ui/layouts/main"
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/" element={<MainPage />} />
-    <Route path="/event/:eventId" element={<EventPage />} />
-    <Route path="*" element={<Navigate to="/" />} />
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<MainPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/event/:eventId" element={<EventPage />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Route>
   </Routes>
 )
 
