7e6e09ac9a325950f3d3953c25e447137923f9c0
Guillaume Boddaert <gboddaert@solocal.com>
2025-03-09 08:42:03 +0100

    chore: added type guards for event entity and write dbo

--- src/resources/domain/dbos/event.write.dbo.ts
+++ src/resources/domain/dbos/event.write.dbo.ts
@@ -5,9 +5,29 @@ import type {
 import type { EventModel } from "~resources/domain/models/event.model"
 
 export type EventWriteDbo = Partial<EventModel> & {
+  id?: string,
   raw_data: {
     wotc: WotcExtractedEvent,
     spreadsheet?: SpreadsheetRawRow[]
     [key: string]: any
   }
+}
+
+/**
+ * Type guard to check if an object conforms to the `EventWriteDbo` type.
+ * Ensures the presence of `raw_data` with a `wotc` field and optionally `spreadsheet`.
+ *
+ * @param obj - The object to validate.
+ * @returns {obj is EventWriteDbo} - `true` if the object matches the type, otherwise `false`.
+ */
+export function isEventWriteDbo(obj: any): obj is EventWriteDbo {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "raw_data" in obj &&
+    typeof obj.raw_data === "object" &&
+    obj.raw_data !== null &&
+    "wotc" in obj.raw_data &&
+    typeof obj.raw_data.wotc === "object"
+  );
 }
