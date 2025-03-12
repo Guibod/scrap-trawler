4d81edda132d93943cf0a827c4a2d368215dea5a
Guillaume Boddaert <guibod12@gmail.com>
2025-03-12 20:15:25 +0100

    chore: main app herouiprovider uses navigate and href from the router

--- src/resources/ui/providers.tsx
+++ src/resources/ui/providers.tsx
@@ -1,11 +1,13 @@
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, useNavigate, useHref } from "react-router-dom"
 import React from "react";
-import { SettingsProvider } from "~resources/ui/providers/settings"
+import { SettingsProvider } from "~/resources/ui/providers/settings"
 
-export const Providers = ({ children }: { children: React.ReactNode }) => {
+export const RoutedProvider = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate()
+
   return (
-    <HeroUIProvider>
+    <HeroUIProvider navigate={navigate} useHref={useHref}>
       <ToastProvider disableAnimation={true} placement="bottom-right" toastProps={{
         radius: "sm",
         severity: "success",
@@ -14,10 +16,18 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
         shouldShowTimeoutProgess: true
       }} />
       <SettingsProvider>
-        <Router>{children}</Router>
+        {children}
       </SettingsProvider>
     </HeroUIProvider>
   );
 };
+
+export const Providers = (props: { children: React.ReactNode }) => (
+  <Router>
+    <RoutedProvider>
+      {props.children}
+    </RoutedProvider>
+  </Router>
+)
 
 export default Providers
