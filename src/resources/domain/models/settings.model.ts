4831f2de78f75f363e6850460313df9c235f8eec
Guillaume Boddaert <guibod12@gmail.com>
2025-03-24 20:56:39 +0100

    chore: removed ability to setup moxfield key since we use our own reverse proxy

--- src/resources/domain/models/settings.model.ts
+++ src/resources/domain/models/settings.model.ts
@@ -2,7 +2,6 @@ import { CardLanguage } from "~/resources/storage/entities/card.entity"
 
 export interface SettingsModel {
   version: number; // Helps track schema evolution
-  moxfieldApiKey: string | null;
   enableCrossEventIdentification: boolean;
   showWelcome: boolean;
   mtgJsonVersion: string | null;
@@ -12,7 +11,6 @@ export interface SettingsModel {
 // Default settings
 export const DEFAULT_SETTINGS: SettingsModel = {
   version: 1,
-  moxfieldApiKey: null,
   enableCrossEventIdentification: false,
   showWelcome: true,
   mtgJsonVersion: null,
