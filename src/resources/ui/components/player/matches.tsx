148faf5c098952e7e8c94bb176b54d6ed6220aa0
Guillaume Boddaert <guibod12@gmail.com>
2025-04-06 23:14:51 +0200

    chore: centered player match items properly if name is too large

--- src/resources/ui/components/player/matches.tsx
+++ src/resources/ui/components/player/matches.tsx
@@ -45,24 +45,23 @@ export function PlayerMatches() {
                 : "default"
 
           return (
-            <div
-              key={match.matchId}
-              className="flex flex-col items-center relative"
-              style={{ width: iconSize }}
-            >
-              <div className="ftext-xs text-center mb-2">Round #{match.round}</div>
-              <Link to={`/event/${event.id}/player/${opponentId}`}>
-                <Avatar
-                  src={opponent?.avatar}
-                  isBordered
-                  color={color}
-                  className="rounded-full"
-                  style={{ width: iconSize, height: iconSize }}
-                />
-                <PlayerName player={opponent} className="text-lg mt-3 block text-center" noIcons={true}/>
-              </Link>
-              <div className="text-xs text-center mt-1">{score}</div>
-            </div>
+            <Link key={match.matchId} to={`/event/${event.id}/player/${opponentId}`}>
+              <div
+                className="flex flex-col items-center relative"
+                style={{ width: iconSize }}
+              >
+                <div className="ftext-xs text-center mb-2">Round #{match.round}</div>
+                  <Avatar
+                    src={opponent?.avatar}
+                    isBordered
+                    color={color}
+                    className="rounded-full"
+                    style={{ width: iconSize, height: iconSize }}
+                  />
+                  <PlayerName player={opponent} className="text-lg mt-3 block text-center" noIcons={true}/>
+                <div className="text-xs text-center mt-1">{score}</div>
+              </div>
+            </Link>
           )
         })}
       </div>
